61a234c484a60f1c9eca15e0d43d205d31c9b45b
Vadym Kalsin <neformat@gmail.com>
2013-12-05 16:56:46 +0200

    Updated locale.php

--- locale.php
+++ locale.php
@@ -1,27 +1,7 @@
 <?php
-//Default to English
-$locale = 'en_US';
-$languages = ($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-$languages = str_replace( ' ', '', $languages );
-$languages = str_replace( '-', '_', $languages );
-$languages = explode( ",", $languages );
 
-//print "Locales = ";
-//print_r($languages);
-//print " <br>";
-
-foreach ( $languages as $temp) {
-	$temp = substr($temp,0,5);
-
-	$trans = './locale/'.$temp.'/LC_MESSAGES/messages.mo';
-
-	//print "Looking for locale in $trans <br>";
-	if ( ($temp == "en_us") || file_exists($trans)) {
-		$locale = $temp;
-		break;
-	}
-
-}
+putenv("LANG=ru_RU");
+$locale = 'ru_RU';
 
 setlocale(LC_ALL, $locale);
 
@@ -30,5 +10,7 @@ bindtextdomain("messages", "./locale");
 
 // Choose domain
 textdomain("messages");
-//echo $locale;
+
+bind_textdomain_codeset("messages", 'UTF-8');
+
 ?>
